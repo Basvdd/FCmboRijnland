530fc8b1c802c1d371473a3525fbb95697eb4c50
Basvdd <00126088@student.rocleiden.nl>
2018-12-19 11:20:25 +0100

    PHP voor toevoegen speler
    niet helemaal functioneel

---
+++ addspeler.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_GET['submit'])){
+    require ('database_connection.php');
+    htmlspecialchars($naam_team = $_POST['teamnaam']);
+    htmlspecialchars($voornaam = $_POST['voornaam']);
+    htmlspecialchars($tussenvoegsel = $_POST['tussenvoegsel']);
+    htmlspecialchars($achternaam = $_POST['achternaam']);
+    htmlspecialchars($leeftijd = $_POST['leeftijd']);
+    $sql = "INSERT INTO speler VALUES (NULL, '$naam_team', '$voornaam', '$tussenvoegsel', '$achternaam', '$leeftijd', NULL)"
+}
+?>
